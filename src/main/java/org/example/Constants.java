b0c9fe1d878f4fda3a8700982d4cd24ce2f1380c
Ilya Golovin <foreachq@gmail.com>
2021-03-27 13:05:41 +0300

    Project constants

---
+++ src/main/java/org/example/Constants.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Constants {
+    public static final String DBNAME = "users";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "root";
+}
