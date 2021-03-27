70148e44f2d8b2ae0560e070d919d6540656f36e
Ilya Golovin <foreachq@gmail.com>
2021-03-27 13:09:20 +0300

    User class

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class User {
+    private String name;
+    private String email;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User: %-20s Email: %-28s Pass: %-15s", getName(), getEmail(), getPassword());
+    }
+}
